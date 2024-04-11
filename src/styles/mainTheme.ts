7b10799fb64bcf0086f74f88e00719939d3f1235
Berta González <bertagonzalezmartin@gmail.com>
2024-04-11 12:21:02 +0200

    Add main theme

---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,20 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  color: {
+    mainDarkColor: "#000",
+    mainLightColor: "#fff",
+    loginBackgroundColor: "#5b5c56",
+    shellColor: "#002e36",
+    buttonColor: "#e3a92b",
+    formColor: "#64b298",
+    positiveToastColor: "#17f448",
+    negativeToastColor: "#f14b4b",
+  },
+  font: {
+    mainFontFamily: "'Fraunces', sans-serif",
+    notFoundFontFamily: "'Schoolbell', cursive",
+  },
+};
+
+export default mainTheme;
