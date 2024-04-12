956a68188a0dfb41d5adb235a0ff77cb648fe84a
Berta González <bertagonzalezmartin@gmail.com>
2024-04-12 12:49:49 +0200

    Add page not found route

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Homepage from "../../pages/Homepage/Homepage";
+import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
 
 const App = (): React.ReactElement => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/home" element={<Homepage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
