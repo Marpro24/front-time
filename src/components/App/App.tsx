a9e0887c37921c35970b51b54e0b87b135a1f3c7
Marta Ibarra <martaa.ibarra@hotmail.com>
2024-04-12 18:20:18 +0200

    Add header to app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,18 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Homepage from "../../pages/Homepage/Homepage";
 import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
+import Header from "../Header/Header";
 
 const App = (): React.ReactElement => {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/home" />} />
-      <Route path="/home" element={<Homepage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Homepage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 };
 
