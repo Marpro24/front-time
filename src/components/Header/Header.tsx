a762d95315fd2cb7ee2c94e197b5cc07c1260123
Marta Ibarra <martaa.ibarra@hotmail.com>
2024-04-17 10:40:54 +0200

    Remove nav menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,8 @@
-import Navigation from "../Navigation/Navigation";
 import HeaderStyled from "./HeaderStyled";
 
 const Header = (): React.ReactElement => {
   return (
     <HeaderStyled>
-      <Navigation />
       <img className="header__logo" src="/images/Logo.png" alt="logo chronos" />
     </HeaderStyled>
   );
